df50d4abd9e83a4fa60f176d261d060213a0acfd
himanshu-Rajoot <himanshu802126@gmail.com>
2025-08-02 20:59:22 +0530

    adding two number

--- src/hello/helloWord.java
+++ src/hello/helloWord.java
@@ -1,7 +1,26 @@
 package hello;
 
+import java.util.Scanner;
+
 public class helloWord {
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+	//	System.out.println("Hello, World!");
+Scanner scanner = new Scanner(System.in);
+
+// Taking first number input
+System.out.print("Enter first number: ");
+int num1 = scanner.nextInt();
+
+// Taking second number input
+System.out.print("Enter second number:");
+int num2 = scanner.nextInt();
+
+// Adding the numbers
+int sum = num1 + num2;
+
+// Displaying the result
+System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+
+scanner.close();
 	}
 }
